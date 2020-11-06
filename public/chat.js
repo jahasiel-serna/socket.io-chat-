edc15c603481d7c77c8ec74823fe1d4cb23eec8d
Jahasiel Serna <jh.serna.b@alumnos.udg.com>
2020-11-05 18:27:13 -0600

    port deleted

--- public/chat.js
+++ public/chat.js
@@ -1,4 +1,4 @@
-const socket = io.connect('https://chat-room-socketio.herokuapp.com/:5000');
+const socket = io.connect('https://chat-room-socketio.herokuapp.com');
 
 const controls = {
     message: document.getElementById('message'),
